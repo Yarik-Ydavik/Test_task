d4965e0bfbce81be932f84b176552e2ce7cd0acc
Yaroslav Zagumennikov <yzagumennikov1@mail.ru>
2024-02-03 12:13:14 +0500

    Initial Commit

---
+++ My_Spar/My_SparApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_SparApp.swift
+//  My_Spar
+//
+//  Created by Yaroslav Zagumennikov on 03.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct My_SparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
